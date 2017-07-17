4ea7838e187ffddcf04ad8def8a2b3a3eeb7665f
karunesh95 <karunesh.upadhyay@quantihpi.com>
2017-07-17 12:09:01 +0530

    first commit

--- Karunesh/Python_day2/Q2.py
+++ Karunesh/Python_day2/Q2.py
@@ -1,7 +1,14 @@
+import logging
+logging.basicConfig(filename='example.log',level=logging.DEBUG)
+logging.debug('This message should go to the log file')
+logging.info('So should this')
+logging.warning('And this, too')
+
+
 t = raw_input("If C to F:C else F.Choose")
 a=int(raw_input("no.?"))
 if t=="C":
-    print (" The temp in Feh is %dF"%(a*1.8+32))
+   print (" The temp in Feh is %dF"%(a*1.8+32))
 elif t=="F":
     print("The temp in Cel is %dC"%(a*(5/9)-(32*5/9)))
 
